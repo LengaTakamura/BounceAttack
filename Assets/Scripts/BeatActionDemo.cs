df9908ca2fbf95d4d627d3d2f367e063292366b0
LENGA_TAKAMURA <kotodaman55324678@gmail.com>
2025-07-30 19:36:42 +0900

    [Add]ビート関連のインターフェースを追加

--- Assets/Scripts/BeatActionDemo.cs
+++ Assets/Scripts/BeatActionDemo.cs
@@ -1,9 +1,10 @@
+using System;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 using System.Threading;
 using CriWare;
 
-public class BeatActionDemo : MonoBehaviour
+public class BeatActionDemo : MonoBehaviour,IBeatSyncListener
 {
     [SerializeField]private string _cueSheetName;
     [SerializeField]private string _acbFilePath;
@@ -12,8 +13,17 @@ public class BeatActionDemo : MonoBehaviour
     private CriAtomExPlayer _atomExPlayer;
     private CancellationTokenSource _cts;
 
+    public int CurrentBpm { get; set; }
+    public int DiffBpm { get; set; }
+    public bool IsBeating { get; set; }
+
     [SerializeField] private GameObject _prefab;
-    private async void Start()
+    private void Start()
+    {
+        Init().Forget();
+        PlayBgm(_bgmCueName);
+    }
+    private async UniTaskVoid Init()
     {
         _cts = new CancellationTokenSource();
 
@@ -26,29 +36,46 @@ public class BeatActionDemo : MonoBehaviour
         await UniTask.WaitUntil(() => !cueSheet.IsLoading, cancellationToken: _cts.Token);
 
         _criAtomExAcb = cueSheet.acb;
-
-        PlayBgm(_bgmCueName);
     }
-
+    
+    private void PlayBgm(string bgmCueName)
+    {
+        if (!_criAtomExAcb.Exists(bgmCueName)) return;
+        _atomExPlayer.SetCue(_criAtomExAcb, bgmCueName);
+        _atomExPlayer.Start();
+    }
+    
     private void OnDestroy()
     {
-        CriAtomExBeatSync.OnCallback -= CriAtomExBeatSync_OnCallback;
         _cts?.Cancel();
         _cts?.Dispose();
     }
 
-    private void PlayBgm(string bgmCueName)
+    private void OnEnable()
     {
-        if (!_criAtomExAcb.Exists(bgmCueName)) return;
-
-        _atomExPlayer.SetCue(_criAtomExAcb, bgmCueName);
-        _atomExPlayer.Start();
-
-        CriAtomExBeatSync.OnCallback += CriAtomExBeatSync_OnCallback;
+       BeatSyncDispatcher.Instance.Register(this);
     }
 
-    private void CriAtomExBeatSync_OnCallback(ref CriAtomExBeatSync.Info info)
+    private void OnDisable()
     {
-         Instantiate(_prefab, transform);
+        BeatSyncDispatcher.Instance.Unregister(this);
     }
+
+    public void OnBeat()
+    {
+        Instantiate(_prefab, transform);
+    }
+}
+
+public interface IBeatSyncListener
+{
+    void OnBeat();
+    
+    int CurrentBpm { get; set;}
+    
+    int DiffBpm { get; set;}
+    
+    bool IsBeating { get; set;}
+    
+    
 }
