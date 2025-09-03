3d2b442500a47509a2acee1cf050f66c94353c88
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-09-03 23:49:24 +0900

    [Update]音ゲー(仮)を実装

--- Assets/Scripts/BeatActionDemo.cs
+++ Assets/Scripts/BeatActionDemo.cs
@@ -1,8 +1,10 @@
 using System;
 using UnityEngine;
-using Cysharp.Threading.Tasks;
 using System.Threading;
 using CriWare;
+using Cysharp.Threading.Tasks;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class BeatActionDemo : MonoBehaviour,IBeatSyncListener
 {
@@ -17,26 +19,33 @@ public class BeatActionDemo : MonoBehaviour,IBeatSyncListener
 
     [SerializeField] private GameObject _prefab;
     [SerializeField]private CriAtomSource _source;
+    private CriAtomExPlayback _playback;
+    [SerializeField] private Image _image;
+    private float _second;
+    private float _nextBeatTime;
+    private CancellationTokenSource _cts2;
     private void Start()
     {
         Init();
         PlayBgm(_bgmCueName);
         BeatSyncDispatcher.Instance.Register(this);
+
     }
     private void Init()
     {
         _cts = new CancellationTokenSource();
+        _cts2 = new CancellationTokenSource();
     }
     
     private void PlayBgm(string bgmCueName)
     {
-        _source.Play(bgmCueName);
+       _playback = _source.Play(bgmCueName);
     }
     
     private void OnDestroy()
     {
-        _cts?.Cancel();
         _cts?.Dispose();
+        _cts2?.Dispose();
     }
     
     private void OnDisable()
@@ -44,15 +53,52 @@ public class BeatActionDemo : MonoBehaviour,IBeatSyncListener
         BeatSyncDispatcher.Instance.Unregister(this);
     }
 
-    public void OnBeat()
+    public void OnBeat(ref CriAtomExBeatSync.Info info)
     {
-        Instantiate(_prefab, transform);
+        Debug.Log("OnBeat");
+        //Instantiate(_prefab, transform);
+        _second = 60f / info.bpm;
+        
+        var nowTime = _playback.GetTimeSyncedWithAudio() / 1000f;
+        _nextBeatTime = nowTime + _second * (1f - info.beatProgress);
+    }
+
+
+    
+    private void Update()
+    {
+        ActionB();
+    }
+
+
+    private void ActionB()
+    {
+        if (!Input.GetKeyDown(KeyCode.B) || !_playback.GetBeatSyncInfo(out CriAtomExBeatSync.Info info)) return;
+        float progress = info.beatProgress; 
+        float bpm = info.bpm;
+        float secondsPerBeat = 60f / bpm;
+        if (progress < 0.1f || progress > 0.9f)
+        {
+            Debug.Log("Great");
+            var obj = Instantiate(_prefab, transform.position, Quaternion.identity);
+            var random = Random.Range(-1f, 1f);
+            var random2 = Random.Range(-1f, 1f);
+            obj.GetComponent<Rigidbody>().AddForce(new Vector3(random,random2,1) * 10, ForceMode.Impulse);
+        }
+        else if (progress < 0.3f || progress > 0.7f)
+        {
+            Debug.Log("Good");
+        }
+        else
+        {
+            Debug.Log("Bad");
+        }
     }
 }
 
 public interface IBeatSyncListener
 {
-    void OnBeat();
+    void OnBeat(ref CriAtomExBeatSync.Info info);
     
     int CurrentBpm { get; set;}
     
