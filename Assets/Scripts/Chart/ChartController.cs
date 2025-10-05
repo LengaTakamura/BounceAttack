39df30de631078ca7d5fa3d926dde54ebfed66de
LENGA_TAKAMURA <kotodaman55324678@gmail.com>
2025-10-05 13:49:50 +0900

    [Add]ノーツ用のクラス作成

---
+++ Assets/Scripts/Chart/ChartController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Chart
+{
+    public class ChartController : MonoBehaviour
+    {
+        
+        public void Init(RectTransform targetRectTransform)
+        {
+            
+        }
+    }
+}
