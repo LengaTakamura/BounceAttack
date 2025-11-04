833994e7c086bd5e6337544abe04dee0029bc394
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-11-05 00:15:49 +0900

    [Update]Hpバー作成中

---
+++ Assets/Scripts/UI/HpBarUi.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class HpBarUi : UiBase
+    {
+        public override void Init(GameEvents gameEvents)
+        {
+            
+        }
+
+        public override void UIOnBeat(BeatInfo info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
